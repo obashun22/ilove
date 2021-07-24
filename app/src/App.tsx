a23e7c4655fcda50f398b9cfbdc9292cfab5ef63
obashun22 <obashun22@yahoo.co.jp>
2021-07-25 01:34:42 +0900

    Update スタイル整形

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+
 import Container from '@material-ui/core/Container';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
+import Box from '@material-ui/core/Box';
+
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    canvas: {
+      width: '95%',
+    },
+    btn: {
+      margin: theme.spacing(1),
+    },
+  }),
+);
 
 const App: React.FC = () => {
+  const classes = useStyles();
+  const [onsyaName, setOnsyaName] = useState<string>("");
+
   return (
     <div className="App">
       {/* header */}
@@ -16,18 +34,28 @@ const App: React.FC = () => {
           <Typography variant="h6">
             I LOVE ONSYA
           </Typography>
-          <Button color="inherit">HIHIHI</Button>
         </Toolbar>
       </AppBar>
 
       {/* main */}
-      <Container maxWidth="sm">
-        <canvas style={{backgroundColor: "pink"}}>
+      <Container maxWidth="sm" style={{backgroundColor: "white"}}>
+        <Box py={3}>
+          <canvas style={{backgroundColor: "pink"}} className={classes.canvas}>
 
-        </canvas>
-        <TextField id="outlined-basic" label="ONSYA NAME" variant="outlined" />
-        <Button variant="contained" color="primary">生成する</Button>
-        <Button variant="contained" color="primary">保存</Button>
+          </canvas>
+        </Box>
+        <Box py={1}>
+          <TextField
+            label="ONSYA NAME"
+            variant="outlined"
+            value={onsyaName}
+            onChange={ e => setOnsyaName(e.target.value) }
+          />
+        </Box>
+        <Box py={1}>
+          <Button variant="contained" color="primary" className={classes.btn}>生成する</Button>
+          <Button variant="contained" color="primary" className={classes.btn}>保存</Button>
+        </Box>
         <p>©2021 shunsuke oba</p>
       </Container>
     </div>
