d90c03e379df52c9e04e0ab34489ff5aaed7c15c
obashun22 <obashun22@yahoo.co.jp>
2021-07-24 23:31:52 +0900

    Update パーツ配置／material-ui追加

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,24 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Container from '@material-ui/core/Container';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* header */}
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">
+            I LOVE ONSYA
+          </Typography>
+          <Button color="inherit">HIHIHI</Button>
+        </Toolbar>
+      </AppBar>
+
+      {/* main */}
+      <Container maxWidth="sm">
+        <canvas style={{backgroundColor: "pink"}}>
+
+        </canvas>
+        <TextField id="outlined-basic" label="ONSYA NAME" variant="outlined" />
+        <Button variant="contained" color="primary">生成する</Button>
+        <Button variant="contained" color="primary">保存</Button>
+        <p>©2021 shunsuke oba</p>
+      </Container>
     </div>
   );
 }
