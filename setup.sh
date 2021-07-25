80c79d2178dc08c538066cff8e1ea00010907048
obashun22 <obashun22@yahoo.co.jp>
2021-07-25 20:45:50 +0900

    Fix プロジェクト名変更

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Reactの環境構築
-docker build . -t onsya
+docker build . -t ilove
 # moduleインストール
-docker run -it --rm -v $(pwd)/app:/usr/src/app onsya yarn install
+docker run -it --rm -v $(pwd)/app:/usr/src/app ilove yarn install
